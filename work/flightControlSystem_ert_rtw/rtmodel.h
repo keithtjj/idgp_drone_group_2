0e6931e0a34ae120f8e01d4f6946283a111b6fb4
LuckyZ <yinzuoliu87@gmail.com>
2025-11-07 00:13:20 +0000

    add

---
+++ work/flightControlSystem_ert_rtw/rtmodel.h
@@ -0,0 +1,19 @@
+/*
+ * rtmodel.h
+ *
+ * Code generation for Simulink model "flightControlSystem".
+ *
+ * Simulink Coder version                : 24.2 (R2024b) 21-Jun-2024
+ * C source code generated on : Thu Nov  6 17:22:05 2025
+ *
+ * Note that the generated code is not dependent on this header file.
+ * The file is used in cojuction with the automatic build procedure.
+ * It is included by the sample main executable harness
+ * MATLAB/rtw/c/src/common/rt_main.c.
+ *
+ */
+
+#ifndef rtmodel_h_
+#define rtmodel_h_
+#include "flightControlSystem.h"
+#endif                                 /* rtmodel_h_ */
